6f0f2ae7d44907d6b6c17c23a533345a6360b83f
rishijivani <rishijivani28@gmail.com>
2025-09-01 12:27:30 +0400

    test

--- lib/core/pitch_service.dart
+++ lib/core/pitch_service.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
-import 'package:fftea/fftea.dart';
 import 'dart:js_interop';
+import 'dart:js_util'; // <-- For promiseToFuture
+import 'package:fftea/fftea.dart';
 import 'note_utils.dart';
 
 @JS()
@@ -54,11 +55,18 @@ class PitchService {
     if (_listening) return;
     _listening = true;
 
-    final nav = Navigator.fromJS(window);
-    final stream = await nav.mediaDevices.getUserMedia(jsify({'audio': true}));
+    // Get navigator.mediaDevices
+    final nav = JS<Navigator>(window);
+    final mediaDevices = nav.mediaDevices;
 
+    // Request microphone access using promiseToFuture
+    final stream = await promiseToFuture<JSAny>(
+      mediaDevices.getUserMedia(JSAny.jsify({'audio': true})),
+    ) as MediaStream;
+
+    // Set up AudioContext
     _audioCtx = AudioContext();
-    _source = _audioCtx!.createMediaStreamSource(stream as MediaStream);
+    _source = _audioCtx!.createMediaStreamSource(stream);
     _analyser = _audioCtx!.createAnalyser();
     _analyser!.fftSize = 2048;
     _source!.connect(_analyser!);
@@ -68,8 +76,10 @@ class PitchService {
     void analyze(num _) {
       _analyser!.getFloatTimeDomainData(buffer);
 
+      // Convert Float32List to List<double> for FFT
       final spectrum = FFT().Transform(buffer.toList());
 
+      // Find peak
       int peakIndex = 0;
       double maxVal = 0;
       for (int i = 0; i < spectrum.length; i++) {
@@ -79,14 +89,16 @@ class PitchService {
         }
       }
 
+      // Calculate frequency
       final freq = peakIndex * _audioCtx!.sampleRate / buffer.length;
       final note = NoteUtils.getClosestNote(freq);
       onNoteDetected(note);
 
-      window.requestAnimationFrame(analyze);
+      // Continue analyzing
+      JS<void>(window)['requestAnimationFrame']!(allowInterop(analyze));
     }
 
-    window.requestAnimationFrame(analyze);
+    JS<void>(window)['requestAnimationFrame']!(allowInterop(analyze));
   }
 
   void stopListening() {
